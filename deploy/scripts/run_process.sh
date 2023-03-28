101532bf92a4d78e6352909937cf4f8cd5944639
gmlcks207 <gmlcks207@gsneotek.com>
2023-03-28 18:18:40 +0900

    Change Jenkins update for codedeploy ch03_05

---
+++ deploy/scripts/run_process.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+REGION="ap-northeast-2"
+ACCOUNT_ID="489527167655"
+ECR_REPOSITORY="${ACCOUNT_ID}.dkr.ecr.${REGION}.amazonaws.com"
+ECR_DOCKER_IMAGE="${ECR_REPOSITORY}/demo"
+ECR_DOCKER_TAG="1.0.1"
+
+aws ecr get-login-password --region ${REGION} \
+  | docker login --username AWS --password-stdin ${ECR_REPOSITORY};
+
+export IMAGE=${ECR_DOCKER_IMAGE};
+export TAG=${ECR_DOCKER_TAG};
+docker-compose -f /home/ec2-user/docker-compose.yml up -d;
